73912e1dc0be6db4025640456a1c6fda3ada803e
Martin Hochenwarter <m.hochenwarter@gmail.com>
2017-09-01 09:38:24 +0200

    Initial commit

---
+++ src/layouts/index.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import withRouter from 'react-router-dom/withRouter'
+import { CSSTransitionGroup } from 'react-transition-group'
+import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+
+import './index.css'
+import './animations.css'
+
+const Header = () => (
+  <div
+    style={{
+      background: 'rebeccapurple',
+      marginBottom: '1.45rem',
+    }}
+  >
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '1.45rem 1.0875rem',
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: 'white',
+            textDecoration: 'none',
+          }}
+        >
+          Gatsby
+        </Link>
+      </h1>
+    </div>
+  </div>
+)
+
+class TransitionHandler extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.location.pathname === window.location.pathname;
+  }
+
+  render() {
+    const {children} = this.props;
+    return (
+      <div className="transition-container">
+        {children}
+      </div>
+    );
+  }
+}
+
+const TemplateWrapper = ({ children, location }) => (
+  <div>
+    <Helmet
+      title="Gatsby Default Starter"
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />
+    <Header />
+    <CSSTransitionGroup
+      component="div"
+      className="transition-group"
+      transitionName="example"
+      transitionEnterTimeout={500}
+      transitionLeaveTimeout={300}
+    >
+      <TransitionHandler
+        location={location}
+        key={location.pathname}
+      >
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '0px 1.0875rem 1.45rem',
+            paddingTop: 0,
+          }}
+        >
+          {children()}
+        </div>
+      </TransitionHandler>
+    </CSSTransitionGroup>
+  </div>
+)
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func,
+}
+
+export default withRouter(TemplateWrapper)
